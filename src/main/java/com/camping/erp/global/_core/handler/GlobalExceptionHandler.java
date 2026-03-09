64279981886aba35ae19c535ed611f705d507121
KimNaKim <gimn02955@gmail.com>
2026-03-09 18:14:35 +0900

    init

--- src/main/java/com/camping/erp/global/_core/handler/GlobalExceptionHandler.java
+++ src/main/java/com/camping/erp/global/_core/handler/GlobalExceptionHandler.java
@@ -1,5 +1,85 @@
 package com.camping.erp.global._core.handler;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.camping.erp.global._core.handler.ex.Exception400;
+import com.camping.erp.global._core.handler.ex.Exception401;
+import com.camping.erp.global._core.handler.ex.Exception403;
+import com.camping.erp.global._core.handler.ex.Exception404;
+import com.camping.erp.global._core.handler.ex.Exception500;
+
+@RestControllerAdvice // 모든 예외를 처리하는 클래스 (Data응답)
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(exception = Exception400.class) // 어떤 예외인지 지정하기
+    public String ex400(Exception400 e) {
+        String html = String.format("""
+                <script>
+                    alert('%s');
+                    history.back();
+                </script>
+                """, e.getMessage());
+        return html;
+    }
+
+    @ExceptionHandler(exception = Exception401.class) // 어떤 예외인지 지정하기
+    public String ex401(Exception401 e) {
+        String html = String.format("""
+                <script>
+                    alert('%s');
+                    location.href = '/login-form';
+                </script>
+                """, e.getMessage());
+        return html;
+    }
+
+    @ExceptionHandler(exception = Exception403.class) // 어떤 예외인지 지정하기
+    public String ex403(Exception403 e) {
+        String html = String.format("""
+                <script>
+                    alert('%s');
+                    history.back();
+                </script>
+                """, e.getMessage());
+        // log남기기
+        return html;
+    }
+
+    @ExceptionHandler(exception = Exception404.class) // 어떤 예외인지 지정하기
+    public String ex404(Exception404 e) {
+        String html = String.format("""
+                <script>
+                    alert('%s');
+                    history.back();
+                </script>
+                """, e.getMessage());
+        return html;
+    }
+
+    @ExceptionHandler(exception = Exception500.class) // 어떤 예외인지 지정하기
+    public String ex500(Exception500 e) {
+        String html = String.format("""
+                <script>
+                    alert('%s');
+                    history.back();
+                </script>
+                """, e.getMessage());
+        return html;
+    }
+
+    @ExceptionHandler(exception = Exception.class) // 어떤 예외인지 지정하기
+    public String exUnknown(Exception e) {
+        String html = String.format("""
+                <script>
+                    alert('%s');
+                    history.back();
+                </script>
+                """, "관리자에게 문의하세요");
+        System.out.println("error : " + e.getMessage());
+        // 1. 로그
+        // 2. SMS알림 -> e.getMessage()
+
+        return html;
+    }
 }
